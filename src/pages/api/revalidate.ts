30ef15428e1ec776960a779e9aae742a862f781c
Willian Lemann <willianleman@hotmail.com>
2022-07-18 12:03:25 -0300

    feature: add revalidate webhook on graphcms

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -5,7 +5,12 @@ export default async function handler(
   response: NextApiResponse
 ) {
   if (request.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
-    return response.status(401).json({ message: "Invalid token" });
+    return response
+      .status(401)
+      .json({
+        message: "Invalid token",
+        token: process.env.REVALIDATE_SECRET_TOKEN,
+      });
   }
 
   try {
